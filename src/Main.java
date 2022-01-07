5e180d13b467be0abe88dc99efc211cc4c7403bb
vp <popovanika@inbox.ru>
2022-01-07 20:41:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = setEndNum();
+        findPrims(num);
+
+    }
+    private static int setEndNum() {
+        System.out.println("Введите конечное число для поиска: ");
+        Scanner in = new Scanner(System.in);
+        int lastNum = in.nextInt();
+        return lastNum;
+    }
+
+    private static void findPrims(int num) {
+        boolean[] primes = new boolean[num + 1];
+        Arrays.fill(primes, true);
+        primes[0] = false;
+        primes[1] = false;
+
+        BitSet bitSet = new BitSet();
+        for (int i = 2; i < primes.length; ++i) {
+            if (primes[i])
+                for (int j = 2; i * j < primes.length; ++j)
+                    primes[i * j] = false;
+            bitSet.set(i);
+        }
+        for (int i = 0; i < primes.length; i++)
+            if (primes[i]) {
+                bitSet.set(i);
+                System.out.print(i + " ");
+            }
+    }
+}
